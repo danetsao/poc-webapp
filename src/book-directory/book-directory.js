9950cac3118cb446c7c9c60488c1cfb568efd9fb
dltsao <dltsao@crimson.ua.edu>
2023-05-25 13:50:21 -0500

    Added book-profile folder and added ng-repeat to display books

--- src/book-directory/book-directory.js
+++ src/book-directory/book-directory.js
@@ -1,4 +1,5 @@
 // Book directory controller
+// Define require
 
 angular.module('app')
 
@@ -13,4 +14,32 @@ angular.module('app')
     .controller('BookDirController', ['$scope', function($scope) {
         $scope.title = 'Book Directory';
         $scope.directive_message = "Here we are in book-directory directive";
+
+        $scope.books_json = [
+            {
+              "title": "The Catcher in the Rye",
+              "author": "J.D. Salinger",
+              "isbn": "0316769177"
+            },
+            {
+              "title": "To Kill a Mockingbird",
+              "author": "Harper Lee",
+              "isbn": "0446310786"
+            },
+            {
+              "title": "1984",
+              "author": "George Orwell",
+              "isbn": "0451524934"
+            },
+            {
+              "title": "The Great Gatsby",
+              "author": "F. Scott Fitzgerald",
+              "isbn": "0743273567"
+            },
+            {
+              "title": "One Hundred Years of Solitude",
+              "author": "Gabriel Garcia Marquez",
+              "isbn": "0060883286"
+            }
+          ];
     }]);
---
+++ src/book-profile/book-profile.js
@@ -0,0 +1,10 @@
+angular.module('app')
+
+    .directive('book_profile', function() {
+        console.log('book-profile directive');
+        return {
+        restrict: 'EA',
+        templateUrl: 'book-profile/book-profile.tpl.html'
+        };
+    }
+    )
